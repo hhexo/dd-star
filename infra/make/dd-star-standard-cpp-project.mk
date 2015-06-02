0f6d1645f090eecf61b9f65266f7e30b912741db
Dario Domizioli <dario.domizioli@gmail.com>
2015-06-02 23:02:12 +0100

    Split off documentation and installation. Modularity changes done.

--- infra/make/dd-star-standard-cpp-project.mk
+++ infra/make/dd-star-standard-cpp-project.mk
@@ -131,84 +131,10 @@ include $(DDSTAR_TOP_LEVEL_DIR)/infra/make/dd-star-system-tests.mk
 # DOCUMENTATION
 # =============================================================================
 
-# Only generate documentation if we have doxygen
-ifeq ($(findstring Doxygen,$(shell $(DOXYGEN) --help)),Doxygen)
-docs: $(PROJECT_DIRECTORY)/build/doc/reference/index.html $(PROJECT_DIRECTORY)/build/doc/documentation/index.html
-else
-docs:
-	@echo "[$(PROJECT_NAME)] No Doxygen - documentation is not generated."
-endif
-
-$(PROJECT_DIRECTORY)/build/doc/reference/index.html: $(ALL_INCLUDES) $(CSRCS) $(CPPSRCS)
-	@echo "[$(PROJECT_NAME)] Generating source documentation with Doxygen..."
-	@mkdir -p $(PROJECT_DIRECTORY)/build/doc
-	@DDSTAR_DOX_PROJ_NAME=$(PROJECT_NAME) $(DOXYGEN) $(DDSTAR_TOP_LEVEL_DIR)/infra/doxy/reference-doxyfile
-
-ALL_DOC_SOURCES := $(wildcard $(PROJECT_DIRECTORY)/doc/*.md) \
-                   $(wildcard $(PROJECT_DIRECTORY)/doc/*/*.md) \
-                   $(wildcard $(PROJECT_DIRECTORY)/doc/*/*/*.md) \
-                   $(wildcard $(PROJECT_DIRECTORY)/doc/*/*/*/*.md) \
-                   $(wildcard $(PROJECT_DIRECTORY)/doc/*/*/*/*/*.md) \
-                   $(wildcard $(PROJECT_DIRECTORY)/doc/*/*/*/*/*/*.md) \
-                   $(wildcard $(PROJECT_DIRECTORY)/doc/*/*/*/*/*/*/*.md) 
-
-# Only generate additional documentation if required.
-ifneq ($(strip $(ALL_DOC_SOURCES)),)
-$(PROJECT_DIRECTORY)/build/doc/documentation/index.html: $(ALL_DOC_SOURCES)
-	@echo "[$(PROJECT_NAME)] Generating additional documentation with Doxygen..."
-	@mkdir -p $(PROJECT_DIRECTORY)/build/doc
-	@DDSTAR_DOX_PROJ_NAME=$(PROJECT_NAME) $(DOXYGEN) $(DDSTAR_TOP_LEVEL_DIR)/infra/doxy/documentation-doxyfile
-else
-$(PROJECT_DIRECTORY)/build/doc/documentation/index.html:
-	@echo "[$(PROJECT_NAME)] No additional documentation to generate..."
-	@mkdir -p $(PROJECT_DIRECTORY)/build/doc/documentation
-	@echo "<html><body>This project has no additional documentation.</body></html>" > $@
-endif
+include $(DDSTAR_TOP_LEVEL_DIR)/infra/make/dd-star-documentation.mk
 
 # =============================================================================
 # INSTALLATION
 # =============================================================================
 
-install: all makeproductdirs install-include install-docs install-test-results
-	@echo "[$(PROJECT_NAME)] Installing libraries..."
-	@cp $(PROJECT_DIRECTORY)/build/lib/release/* $(DDSTAR_TOP_LEVEL_DIR)/product/lib/
-	@cp $(PROJECT_DIRECTORY)/build/lib/debug/* $(DDSTAR_TOP_LEVEL_DIR)/product/lib/debug/
-	@cp $(PROJECT_DIRECTORY)/build/lib/checking/* $(DDSTAR_TOP_LEVEL_DIR)/product/lib/checking/
-	@echo "[$(PROJECT_NAME)] Installing binaries..."
-	@if [ -d $(PROJECT_DIRECTORY)/build/bin/release ]; then cp $(PROJECT_DIRECTORY)/build/bin/release/* $(DDSTAR_TOP_LEVEL_DIR)/product/bin/ ; fi
-	@if [ -d $(PROJECT_DIRECTORY)/build/bin/debug ]; then cp $(PROJECT_DIRECTORY)/build/bin/debug/* $(DDSTAR_TOP_LEVEL_DIR)/product/bin/debug/ ; fi
-	@if [ -d $(PROJECT_DIRECTORY)/build/bin/checking ]; then cp $(PROJECT_DIRECTORY)/build/bin/checking/* $(DDSTAR_TOP_LEVEL_DIR)/product/bin/checking/ ; fi
-	@echo "[$(PROJECT_NAME)] Installed in $(DDSTAR_TOP_LEVEL_DIR)/product."
-
-makeproductdirs:
-	@mkdir -p $(DDSTAR_TOP_LEVEL_DIR)/product/bin
-	@mkdir -p $(DDSTAR_TOP_LEVEL_DIR)/product/bin/debug
-	@mkdir -p $(DDSTAR_TOP_LEVEL_DIR)/product/bin/checking
-	@mkdir -p $(DDSTAR_TOP_LEVEL_DIR)/product/lib
-	@mkdir -p $(DDSTAR_TOP_LEVEL_DIR)/product/lib/debug
-	@mkdir -p $(DDSTAR_TOP_LEVEL_DIR)/product/lib/checking
-	@mkdir -p $(DDSTAR_TOP_LEVEL_DIR)/product/include
-	@mkdir -p $(DDSTAR_TOP_LEVEL_DIR)/product/doc/$(PROJECT_NAME)/reference
-	@mkdir -p $(DDSTAR_TOP_LEVEL_DIR)/product/doc/$(PROJECT_NAME)/documentation
-	@mkdir -p $(DDSTAR_TOP_LEVEL_DIR)/product/test-results/unit
-	@mkdir -p $(DDSTAR_TOP_LEVEL_DIR)/product/test-results/system
-
-install-include: makeproductdirs
-	@echo "[$(PROJECT_NAME)] Installing include files..."
-	@cp -r $(PROJECT_DIRECTORY)/include/* $(DDSTAR_TOP_LEVEL_DIR)/product/include/
-
-# Only generate documentation if we have doxygen
-ifeq ($(findstring Doxygen,$(shell $(DOXYGEN) --help)),Doxygen)
-install-docs: docs makeproductdirs
-	@echo "[$(PROJECT_NAME)] Installing documentation..."
-	@cp -r $(PROJECT_DIRECTORY)/build/doc/reference/* $(DDSTAR_TOP_LEVEL_DIR)/product/doc/$(PROJECT_NAME)/reference/
-	@cp -r $(PROJECT_DIRECTORY)/build/doc/documentation/* $(DDSTAR_TOP_LEVEL_DIR)/product/doc/$(PROJECT_NAME)/documentation/
-else
-install-docs:
-	@echo "[$(PROJECT_NAME)] No Doxygen - documentation is not installed."
-endif
-
-install-test-results: tests makeproductdirs
-	@echo "[$(PROJECT_NAME)] Installing test results..."
-	@cp -r $(PROJECT_DIRECTORY)/build/test/unit/results/* $(DDSTAR_TOP_LEVEL_DIR)/product/test-results/unit/
-	@cp -r $(PROJECT_DIRECTORY)/build/test/system/results/* $(DDSTAR_TOP_LEVEL_DIR)/product/test-results/system/
+include $(DDSTAR_TOP_LEVEL_DIR)/infra/make/dd-star-installation.mk
