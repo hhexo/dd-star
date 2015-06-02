f6cd47db771e2a1c6ac86e4c406a752494c58a32
Dario Domizioli <dario.domizioli@gmail.com>
2015-06-02 22:46:48 +0100

    Split off cleanup and builds...

--- infra/make/dd-star-standard-cpp-project.mk
+++ infra/make/dd-star-standard-cpp-project.mk
@@ -95,116 +95,13 @@ include $(DDSTAR_TOP_LEVEL_DIR)/infra/make/dd-star-source-dependencies-generatio
 # CLEANUP
 # =============================================================================
 
-clean: clean-build clean-doc clean-tests
-	@echo "[$(PROJECT_NAME)] Cleaned."
-
-clean-build: clean-debug clean-release clean-checking
-
-clean-debug:
-	rm -fr $(PROJECT_DIRECTORY)/build/lib/debug
-	rm -fr $(PROJECT_DIRECTORY)/build/bin/debug
-	rm -fr $(PROJECT_DIRECTORY)/build/obj/debug
-
-clean-release:
-	rm -fr $(PROJECT_DIRECTORY)/build/lib/release
-	rm -fr $(PROJECT_DIRECTORY)/build/bin/release
-	rm -fr $(PROJECT_DIRECTORY)/build/obj/release
-
-clean-checking:
-	rm -fr $(PROJECT_DIRECTORY)/build/lib/checking
-	rm -fr $(PROJECT_DIRECTORY)/build/bin/checking
-	rm -fr $(PROJECT_DIRECTORY)/build/obj/checking
-
-clean-doc:
-	rm -fr $(PROJECT_DIRECTORY)/build/doc/*
-
-clean-tests:
-	rm -fr $(PROJECT_DIRECTORY)/build/test/unit
-	rm -fr $(PROJECT_DIRECTORY)/build/test/system
+include $(DDSTAR_TOP_LEVEL_DIR)/infra/make/dd-star-clean.mk
 
 # =============================================================================
-# DEBUG BUILD
+# BUILDS: DEBUG, CHECKING AND RELEASE
 # =============================================================================
 
-DEBUG_PROGRAMS = $(patsubst %,$(PROJECT_DIRECTORY)/build/bin/debug/%,$(TARGET_PROGRAMS))
-debug: $(PROJECT_DIRECTORY)/build/lib/debug/$(TARGET_LIBRARY) $(DEBUG_PROGRAMS)
-
-$(PROJECT_DIRECTORY)/build/lib/debug/$(TARGET_LIBRARY): $(LIBOBJS_DEBUG)
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Creating static library $@ for Debug..."
-	@$(AR) rcs $@ $^
-
-$(PROJECT_DIRECTORY)/build/bin/debug/%: $(PROJECT_DIRECTORY)/build/obj/debug/%.opp $(PROJECT_DIRECTORY)/build/lib/debug/$(TARGET_LIBRARY)
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Linking executable $@ for Debug..."
-	@$(LD) $^ $(LDFLAGS_DEBUG) -o $@
-
-# Debug objs production
-$(PROJECT_DIRECTORY)/build/obj/debug/%.o: $(PROJECT_DIRECTORY)/src/%.c
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Compiling $< for Debug..."
-	@$(CC) -c $(CFLAGS_DEBUG) $< -o $@
-
-$(PROJECT_DIRECTORY)/build/obj/debug/%.opp: $(PROJECT_DIRECTORY)/src/%.cpp
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Compiling $< for Debug..."
-	@$(CXX) -c $(CXXFLAGS_DEBUG) $< -o $@
-
-# =============================================================================
-# RELEASE BUILD
-# =============================================================================
-
-RELEASE_PROGRAMS = $(patsubst %,$(PROJECT_DIRECTORY)/build/bin/release/%,$(TARGET_PROGRAMS))
-release: $(PROJECT_DIRECTORY)/build/lib/release/$(TARGET_LIBRARY) $(RELEASE_PROGRAMS)
-
-$(PROJECT_DIRECTORY)/build/lib/release/$(TARGET_LIBRARY): $(LIBOBJS_RELEASE)
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Creating static library $@ for Release..."
-	@$(AR) rcs $@ $^
-
-$(PROJECT_DIRECTORY)/build/bin/release/%: $(PROJECT_DIRECTORY)/build/obj/release/%.opp $(PROJECT_DIRECTORY)/build/lib/release/$(TARGET_LIBRARY)
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Linking executable $@ for Release..."
-	@$(LD) $^ $(LDFLAGS_RELEASE) -o $@
-
-# Release objs production
-$(PROJECT_DIRECTORY)/build/obj/release/%.o: $(PROJECT_DIRECTORY)/src/%.c
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Compiling $< for Release..."
-	@$(CC) -c $(CFLAGS_RELEASE) $< -o $@
-
-$(PROJECT_DIRECTORY)/build/obj/release/%.opp: $(PROJECT_DIRECTORY)/src/%.cpp
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Compiling $< for Release..."
-	@$(CXX) -c $(CXXFLAGS_RELEASE) $< -o $@
-
-# =============================================================================
-# CHECKING BUILD
-# =============================================================================
-
-CHECKING_PROGRAMS = $(patsubst %,$(PROJECT_DIRECTORY)/build/bin/checking/%,$(TARGET_PROGRAMS))
-checking: $(PROJECT_DIRECTORY)/build/lib/checking/$(TARGET_LIBRARY) $(CHECKING_PROGRAMS)
-
-$(PROJECT_DIRECTORY)/build/lib/checking/$(TARGET_LIBRARY): $(LIBOBJS_CHECKING)
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Creating static library $@ for Checking..."
-	@$(AR) rcs $@ $^
-
-$(PROJECT_DIRECTORY)/build/bin/checking/%: $(PROJECT_DIRECTORY)/build/obj/checking/%.opp $(PROJECT_DIRECTORY)/build/lib/checking/$(TARGET_LIBRARY)
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Linking executable $@ for Checking..."
-	@$(LD) $^ $(LDFLAGS_CHECKING) -o $@
-
-# Checking objs production
-$(PROJECT_DIRECTORY)/build/obj/checking/%.o: $(PROJECT_DIRECTORY)/src/%.c
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Compiling $< for Checking..."
-	@$(CC) -c $(CFLAGS_CHECKING) $< -o $@
-
-$(PROJECT_DIRECTORY)/build/obj/checking/%.opp: $(PROJECT_DIRECTORY)/src/%.cpp
-	@mkdir -p $(dir $@)
-	@echo "[$(PROJECT_NAME)] Compiling $< for Checking..."
-	@$(CXX) -c $(CXXFLAGS_CHECKING) $< -o $@
+include $(DDSTAR_TOP_LEVEL_DIR)/infra/make/dd-star-build.mk
 
 # =============================================================================
 # TESTS
