67c04916c38f71c3af48667a78094c9d9b392a56
Dario Domizioli <dario.domizioli@gmail.com>
2015-06-02 22:34:02 +0100

    Split off sources and final targets identification...

--- infra/make/dd-star-standard-cpp-project.mk
+++ infra/make/dd-star-standard-cpp-project.mk
@@ -80,52 +80,10 @@ include $(DDSTAR_TOP_LEVEL_DIR)/infra/make/dd-star-compile-flags.mk
 
 
 # =============================================================================
-# SOURCES AND TARGETS
+# SOURCES AND FINAL TARGET NAMES
 # =============================================================================
 
-ALL_INCLUDES := $(wildcard $(PROJECT_DIRECTORY)/include/*.h) \
-                $(wildcard $(PROJECT_DIRECTORY)/include/*/*.h) \
-                $(wildcard $(PROJECT_DIRECTORY)/include/*/*/*.h) \
-                $(wildcard $(PROJECT_DIRECTORY)/include/*/*/*/*.h) \
-                $(wildcard $(PROJECT_DIRECTORY)/include/*/*/*/*/*.h) \
-                $(wildcard $(PROJECT_DIRECTORY)/include/*/*/*/*/*/*.h) \
-                $(wildcard $(PROJECT_DIRECTORY)/include/*/*/*/*/*/*/*.h)
-
-CSRCS := $(wildcard $(PROJECT_DIRECTORY)/src/*.c) \
-         $(wildcard $(PROJECT_DIRECTORY)/src/*/*.c) \
-         $(wildcard $(PROJECT_DIRECTORY)/src/*/*/*.c) \
-         $(wildcard $(PROJECT_DIRECTORY)/src/*/*/*/*.c) \
-         $(wildcard $(PROJECT_DIRECTORY)/src/*/*/*/*/*.c) \
-         $(wildcard $(PROJECT_DIRECTORY)/src/*/*/*/*/*/*.c) \
-         $(wildcard $(PROJECT_DIRECTORY)/src/*/*/*/*/*/*/*.c)
-
-CPPSRCS := $(wildcard $(PROJECT_DIRECTORY)/src/*.cpp) \
-           $(wildcard $(PROJECT_DIRECTORY)/src/*/*.cpp) \
-           $(wildcard $(PROJECT_DIRECTORY)/src/*/*/*.cpp) \
-           $(wildcard $(PROJECT_DIRECTORY)/src/*/*/*/*.cpp) \
-           $(wildcard $(PROJECT_DIRECTORY)/src/*/*/*/*/*.cpp) \
-           $(wildcard $(PROJECT_DIRECTORY)/src/*/*/*/*/*/*.cpp) \
-           $(wildcard $(PROJECT_DIRECTORY)/src/*/*/*/*/*/*/*.cpp)
-
-# Objects
-COBJS_DEBUG = $(patsubst $(PROJECT_DIRECTORY)/src/%.c,$(PROJECT_DIRECTORY)/build/obj/debug/%.o,$(CSRCS))
-CPPOBJS_DEBUG = $(patsubst $(PROJECT_DIRECTORY)/src/%.cpp,$(PROJECT_DIRECTORY)/build/obj/debug/%.opp,$(CPPSRCS))
-COBJS_RELEASE = $(patsubst $(PROJECT_DIRECTORY)/src/%.c,$(PROJECT_DIRECTORY)/build/obj/release/%.o,$(CSRCS))
-CPPOBJS_RELEASE = $(patsubst $(PROJECT_DIRECTORY)/src/%.cpp,$(PROJECT_DIRECTORY)/build/obj/release/%.opp,$(CPPSRCS))
-COBJS_CHECKING = $(patsubst $(PROJECT_DIRECTORY)/src/%.c,$(PROJECT_DIRECTORY)/build/obj/checking/%.o,$(CSRCS))
-CPPOBJS_CHECKING = $(patsubst $(PROJECT_DIRECTORY)/src/%.cpp,$(PROJECT_DIRECTORY)/build/obj/checking/%.opp,$(CPPSRCS))
-
-MAINOBJS_DEBUG := $(patsubst $(PROJECT_DIRECTORY)/src/%.cpp,$(PROJECT_DIRECTORY)/build/obj/debug/%.opp,$(realpath $(PROJECT_MAIN_SRCS)))
-MAINOBJS_RELEASE := $(patsubst $(PROJECT_DIRECTORY)/src/%.cpp,$(PROJECT_DIRECTORY)/build/obj/release/%.opp,$(realpath $(PROJECT_MAIN_SRCS)))
-MAINOBJS_CHECKING := $(patsubst $(PROJECT_DIRECTORY)/src/%.cpp,$(PROJECT_DIRECTORY)/build/obj/checking/%.opp,$(realpath $(PROJECT_MAIN_SRCS)))
-
-LIBOBJS_DEBUG = $(filter-out $(MAINOBJS_DEBUG),$(CPPOBJS_DEBUG) $(COBJS_DEBUG))
-LIBOBJS_RELEASE = $(filter-out $(MAINOBJS_RELEASE),$(CPPOBJS_RELEASE) $(COBJS_RELEASE))
-LIBOBJS_CHECKING = $(filter-out $(MAINOBJS_CHECKING),$(CPPOBJS_CHECKING) $(COBJS_CHECKING))
-
-# Final targets
-TARGET_LIBRARY := lib$(PROJECT_NAME).a
-TARGET_PROGRAMS := $(patsubst $(PROJECT_DIRECTORY)/src/%.cpp,%,$(abspath $(PROJECT_MAIN_SRCS)))
+include $(DDSTAR_TOP_LEVEL_DIR)/infra/make/dd-star-sources-and-targets.mk
 
 # =============================================================================
 # SOURCE DEPENDENCIES CONSTRUCTION
